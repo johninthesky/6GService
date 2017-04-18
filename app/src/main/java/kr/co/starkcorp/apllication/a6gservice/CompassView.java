fdf772f2124002c43a2ef49a597f89320c14e160
DESKTOP-ND8N362\junghyun <junghyun@starkcorp.co.kr>
2017-04-18 10:15:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/starkcorp/apllication/a6gservice/CompassView.java
@@ -0,0 +1,43 @@
+package kr.co.starkcorp.apllication.a6gservice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+
+/**
+ * Created by junghyun on 2017-04-18.
+ */
+
+public class CompassView  extends View{
+    private Drawable mCompass;
+    private float mAzimuth = 0;
+    private int PADDING = 2;
+
+    public CompassView(Context ctx) {
+        super(ctx);
+
+        this.mCompass = ctx.getResources().getDrawable(R.drawable.arrow_n);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+
+        canvas.rotate(360 - mAzimuth, PADDING + mCompass.getMinimumWidth()
+                / 2, PADDING + mCompass.getMinimumHeight() / 2);
+        mCompass.setBounds(PADDING, PADDING, PADDING
+                + mCompass.getMinimumWidth(), PADDING
+                + mCompass.getMinimumHeight());
+
+        mCompass.draw(canvas);
+        canvas.restore();
+
+        super.onDraw(canvas);
+    }
+
+    public void setAzimuth(float aAzimuth) {
+        mAzimuth = aAzimuth;
+    }
+
+}
